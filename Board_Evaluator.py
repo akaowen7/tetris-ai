b24e012e29d87d6aa749e5739fa28c45ced5067d
Max Zimmermann <hobomojo3@gmail.com>
2025-01-18 15:31:18 -0500

    Board_Eval progress + Renaming misspelled file

--- Board_Evaluator.py
+++ Board_Evaluator.py
@@ -1,18 +1,41 @@
 import numpy as np
+import Permutation_Generator as Perm_Gen
+
+sample_board = np.array([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0], 
+    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0], 
+    [1, 1, 1, 0, 0, 0, 1, 1, 1, 0], 
+    [1, 0, 1, 1, 1, 0, 1, 1, 1, 0], 
+    [1, 0, 0, 1, 0, 1, 1, 1, 1, 0], 
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0]])
 
 class Board_Evaluator:
     def __init__(self, initial_board):
-        self.initial_board = initial_board # 10 x 20 np array
+        self.initial_board = initial_board # 20 x 10 np array
     
     # Factor parameters
-    holes_param = 1
-    height_param = 1
-    variance_param = 1
-    lines_param = 1
+    holes_param = 1 # a
+    height_param = 1 # b
+    variance_param = 1 # c
+    lines_param = 1 # d
 
     def find_next_move(self):
         # calc all permutations of next piece
-        perms = [0,0,0,0,0,0]
+        perms = Perm
 
         calculated_values = []
         for board in perms:
@@ -20,25 +43,56 @@ class Board_Evaluator:
 
         calculated_values.sort(key=lambda x: x[1])
 
-        # returns only the board NEED TO FIND THE MOVE
+        # returns only the board, WE NEED TO FIND THE MOVE
         return calculated_values[0][0]
 
 
     def find_board_value(self, board):
         return (self.holes(board) * self.holes_param) + (self.board_height(board) * self.height_param) + (self.surface_variance(board) * self.variance_param) +(self.complete_lines(board) * self.lines_param)
 
-    def holes(_, board):
+    def holes(_, board: np.array):
         return 1
 
-    def board_height(_, board):
-        return 1
+    def board_height(_, board: np.array):
+        max_height = 0
+
+        for i in range(board.shape[1]):
+            col = board[:, i] # Gets columns
+
+            print(col)
+
+            blocksInCol = np.where(col == 1)[0]
+            if blocksInCol.size > 0 and (board.shape[0] - blocksInCol[0]) > max_height:
+                max_height = board.shape[0] - blocksInCol[0]
+
+        return max_height - 1 # Subtract 1 to make it 0-indexed
+    
 
     def surface_variance(_, board):
-        return 1
+        colHeights = []
+        for i in range(board.shape[1]):
+            col = board[:, i] # Gets columns
+
+            blocksInCol = np.where(col == 1)[0]
+            height = 0
+            if blocksInCol.size > 0:
+                height = board.shape[0] - blocksInCol[0]
+            colHeights.append(height)
+
+        print(colHeights)
+        
+        return sum(abs(colHeights[i] - colHeights[i + 1]) for i in range(len(colHeights) - 1))
 
     def complete_lines(_, board):
-        return 1
-    
-test = Board_Evaluator(0)
+        completeLineCount = 0
 
-print(test.find_board_value(test.initial_board))
+        for row in board:
+            if 0 not in row:
+                completeLineCount += 1
+        return completeLineCount
+    
+boardEvalObj = Board_Evaluator(sample_board)
+
+print("Max height: ", boardEvalObj.board_height(sample_board))
+print("Surface variance: ", boardEvalObj.surface_variance(sample_board))
+print("Complete lines: ", boardEvalObj.complete_lines(sample_board))
